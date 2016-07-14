beb52bd88809c24e9173b934cf4233dedb44a7d2
James Orr <orrjamesw@gmail.com>
2016-07-14 17:47:43 -0400

    Refactors main.

--- app/Main.hs
+++ app/Main.hs
@@ -6,33 +6,29 @@ import qualified Data.ByteString.Char8 as B
 
 import Referee
 import Referee.Examples.RockPaperScissors
+import Referee.Config (Config)
 import qualified Referee.Config as Config
+
+runApp :: Config -> IO ()
+runApp config = do
+  userMap <- newUserMap
+  mmMap <- newMatchmakingMap
+  loginMap <- newLoginMap
+  gameMap <- newGameMap
+  let userHandler = inMemoryUserHandler userMap
+      matchmakingHandler = inMemoryMatchmakingHandler mmMap
+      loginHandler = inMemoryLoginHandler loginMap
+      gameHandler = inMemoryGameHandler rpsRules gameMap
+      secret = Config._jwtSecret config
+      cost = Config._bcryptCost config
+      allApp = allApplication userHandler matchmakingHandler loginHandler gameHandler secret cost
+  run 8081 allApp
 
 main :: IO ()
 main = do
   -- load the environment from a .env file, if one exists
   Config.seedEnv
   -- read configuration from environment variables
-  eConfig <- Config.getConfig
-  case eConfig of
-    Left complaint -> do
-      putStrLn "Couldn't generate required config from the environment."
-      putStrLn "Here's the complaint the config library threw:"
-      putStrLn complaint
-    Right config -> do
-      -- generate javascript for calling the api
-      -- writeJSForAPI allRoutes vanillaJS "api.js"
-      -- initialize the various modules
-      userMap <- newUserMap
-      mmMap <- newMatchmakingMap
-      loginMap <- newLoginMap
-      gameMap <- newGameMap
-      let userHandler = inMemoryUserHandler userMap
-          matchmakingHandler = inMemoryMatchmakingHandler mmMap
-          loginHandler = inMemoryLoginHandler loginMap
-          gameHandler = inMemoryGameHandler rpsRules gameMap
-          secret = Config._jwtSecret config
-          cost = Config._bcryptCost config
-          allApp = allApplication userHandler matchmakingHandler loginHandler gameHandler secret cost
-      -- run the app
-      run 8081 allApp
+  errorOrConfig <- Config.getConfig
+  -- run the app, if possible
+  either putStrLn runApp errorOrConfig
