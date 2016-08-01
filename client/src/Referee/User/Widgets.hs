729f4dbcef3d71d6eb33c2a34f712d7f24e1e376
James Orr <orrjamesw@gmail.com>
2016-07-31 21:08:13 -0400

    Adds password widget.
    This is hardly worth a commit, but was fun and my first reflex widget.

--- client/src/Referee/User/Widgets.hs
+++ client/src/Referee/User/Widgets.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MultiWayIf #-}
+
 module Referee.User.Widgets where
 
 import Reflex
@@ -5,14 +7,39 @@ import Reflex.Dom
 import Servant.Reflex
 import Servant.API
 import Data.Proxy
+import Data.Maybe (fromJust, isJust)
 
 import Referee.User.Routes
 
--- better would be passwordWidget :: MonadWidget t m => m (Maybe Password)
--- that validates some policy and that the PWs match
--- and the widget has both password and confirmation
-passwordWidget :: MonadWidget t m => m (TextInput t)
-passwordWidget = textInput (def { _textInputConfig_inputType = "password"})
+data PasswordError = InputsDontMatch | FailsPolicy String
+  deriving Show
+
+data PasswordResult = ValidPW PlaintextPassword | InvalidPW PasswordError
+  deriving Show
+
+type PlaintextPassword = String
+
+-- | passwordWidget sticks two password inputs into the DOM
+-- and has a value that's Just password if they match and pass the policy function.
+-- policy should return Nothing if the password is valid,
+-- or Just errorMessage otherwise.
+passwordWidget :: MonadWidget t m => (String -> Maybe String) -> m (Dynamic t PasswordResult)
+passwordWidget policy = do
+  passwordInput <- textInput (def { _textInputConfig_inputType = "password"})
+  passwordConfirm <- textInput (def { _textInputConfig_inputType = "password"})
+
+  let inputVal = _textInput_value passwordInput
+      confirmVal = _textInput_value passwordConfirm
+
+  inputsMatch <- combineDyn (==) inputVal confirmVal
+  policyResult <- mapDyn policy inputVal
+  -- validInput <- combineDyn (\match input -> if match  && policy input then Just input else Nothing) inputsMatch inputVal
+  passwordResult <- combineDyn (\match input ->
+    if | not match -> InvalidPW InputsDontMatch
+       | isJust (policy input) -> InvalidPW (FailsPolicy (fromJust (policy input)))
+       | otherwise -> ValidPW input) inputsMatch inputVal
+
+  return passwordResult
 
 registerWidget :: MonadWidget t m => m ()
 registerWidget = do
@@ -24,13 +51,13 @@ registerWidget = do
 
   regNameInput <- textInput def
   regEmailInput <- textInput def
-  regPasswordInput <- passwordWidget
-  regPasswordConfInput <- passwordWidget
-
-
+  regPassword <- passwordWidget (const Nothing)
 
   registerButton <- button "register"
 
   users <- getUsers registerButton
+
+  pwShow <- mapDyn show regPassword
+  dynText pwShow
 
   return ()
