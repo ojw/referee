8587baa8d2ff0ec43bbe53cafa4179ef03ec8bca
James Orr <orrjamesw@gmail.com>
2016-08-06 23:16:30 -0400

    Fixes build order.
    `build.sh` was building the server, moving the client files, then
    building the client.  What a goof!

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env sh
 
-stack build --stack-yaml=server/server-stack.yaml
+stack build --stack-yaml=client/client-stack.yaml
 
 rm -f static/all.js
 cp -r $(stack path --stack-yaml=client/client-stack.yaml --local-install-root)/bin/referee-client-exe.jsexe/all.js static/all.js
 
-stack build --stack-yaml=client/client-stack.yaml
+stack build --stack-yaml=server/server-stack.yaml
